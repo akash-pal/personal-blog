c493a61fd711664410a4af667c19f2c19fa7e655
Akash Pal <akashpa93@gmail.com>
2025-12-25 23:19:20 +0530

    fix: update PostCSS config and add Tailwind CSS configuration file

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   output: 'standalone',
+   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
 };
 
 export default nextConfig;
---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+const config = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './data/**/*.{md,mdx}',
+    './scripts/**/*.{js,ts}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
